7b4867936d1b6f973bd2a6d9e3f28cfe17350446
thaddeusa <thaddeusa@gmail.com>
2018-02-09 15:01:44 -0700

    correct spelling

--- simple-standalone/ec2.tf
+++ simple-standalone/ec2.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "amz-linux" {
     ami = "${lookup(var.amis_amzlinux, var.aws_region)}"
     instance_type = "${var.instance_type}"
     key_name = "${var.aws_key_name}"
-    vpc_security_group_ids = ["${aws_security_group.amz-linix-sg.id}"]
+    vpc_security_group_ids = ["${aws_security_group.amz-linux-sg.id}"]
     subnet_id = "${aws_subnet.PublicSubnet_az1.id}"
     associate_public_ip_address = true
     tags {
-        Name = "${var.environment}-amz-linix"
+        Name = "${var.environment}-amz-linux"
     }
   }
