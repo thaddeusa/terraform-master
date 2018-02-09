852b3240240f66620f8249e69ecc6f46b05f3381
thaddeusa <thaddeusa@gmail.com>
2018-02-09 15:12:23 -0700

    remove output for security group

--- simple-standalone/outputs.tf
+++ simple-standalone/outputs.tf
@@ -1,9 +1,6 @@
 output "vpc1_id" {
   value = "${aws_vpc.vpc1.id}"
 }
-output "amz_linix_sg_id" {
-  value = "${aws_security_group.amz-linix-sg.id}"
-}
 output "public_subnet_az1_id" {
   value = "${aws_subnet.PublicSubnet_az1.id}"
 }
